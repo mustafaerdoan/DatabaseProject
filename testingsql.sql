7a7f8021b2372417b23eb2aa194dfe00877224d5
Mustafa Erdogan <c.mustafaerdoann@hotmail.com>
2019-12-26 18:12:51 +0300

    TestingQuery

---
+++ testingsql.sql
@@ -0,0 +1,18 @@
+USE RailwayDB;
+
+
+--! Where you can view the tables and see that they are actually updating (when you execute) 
+SELECT *
+FROM ADMIN;
+SELECT *
+FROM PASSENGER;
+SELECT *
+FROM TRAIN;
+SELECT *
+FROM TRAIN_STATION;
+SELECT *
+FROM SCHEDULE;
+SELECT *
+FROM INFO;
+SELECT *
+FROM TICKET;
